fb4fcbc30b25669dadeed350d8d6611c515bd440
davidchermanto <davidchermanto@gmail.com>
2022-11-22 00:53:18 +0800

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Interval per ice block
+    private float intervalX = 0.4f;
+
+    private float initX = -1.5f;
+    private float initY = -4.2f;
+
+    private int currentId = 0;
+    private int furthestActivatedIceId = 0;
+
+    private GameObject iceBlockPrefab;
+    private List<IceBlock> iceblocks;
+
+    public bool isPlaying;
+
+    private float maxInitialIceDurability = 60;
+    private float minInitialIceDurability = 5;
+
+    private float initialDurabilityPoints = 50;
+
+    private float minimumDurabilityPoints = 10;
+    private float durabilityVariation = 7;
+
+    private float durabilityReducedPerGeneration = 0.1f;
+
+    private float initialGenerationCount = 150;
+
+    public void StartGame()
+    {
+        isPlaying = true;
+
+    }
+
+    public void GenerateStartingIce()
+    {
+        for(int i = 0; i < initialGenerationCount; i++)
+        {
+            GenerateIce();
+        }
+    }
+
+    // Generate ice uses current id to determine where to generate the ice.
+    public void GenerateIce()
+    {
+        if (isPlaying)
+        {
+            int id = currentId;
+
+            float newPosX = id * intervalX + initX;
+            float newPosY = initY;
+
+            GameObject newIce = Instantiate(iceBlockPrefab);
+            IceBlock iceBlock = newIce.GetComponent<IceBlock>();
+
+            newIce.transform.position = new Vector3(newPosX, newPosY, 0);
+            iceblocks.Add(iceBlock);
+        }
+    }
+
+    public float GetRandomizedDurability()
+    {
+        float baseDurability = initialDurabilityPoints - currentId * durabilityReducedPerGeneration;
+        if(baseDurability < minimumDurabilityPoints) { baseDurability = minimumDurabilityPoints; }
+
+        float randomMod = Random.Range(0, durabilityVariation);
+        if(Random.Range(0, 2) == 0)
+        {
+            randomMod = -randomMod;
+        }
+
+        float finDurability = baseDurability + randomMod;
+
+        if(finDurability < minInitialIceDurability) 
+        { 
+            finDurability = minInitialIceDurability; 
+        }
+
+        if(finDurability > maxInitialIceDurability) 
+        { 
+            finDurability = maxInitialIceDurability; 
+        }
+
+        return finDurability;
+    }
+
+    public void UpdateFurthestActiveIce(int id)
+    {
+        if(id > furthestActivatedIceId)
+        {
+            furthestActivatedIceId = id;
+        }
+    }
+}
---
+++ Assets/Scripts/IceBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBlock : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D iceCollider;
+
+    // 0 to 100. When reaches 0, ice dies
+    [SerializeField] private float durability;
+
+    // What number of ice is this
+    [SerializeField] private int id;
+
+    [SerializeField] private Color color;
+
+    void InitIce(int id, float durability)
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+}
